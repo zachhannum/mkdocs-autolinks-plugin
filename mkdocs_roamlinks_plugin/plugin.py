68e2d8f9ac24a6cd8312ffbb94ac4dd79c799af4
jackiexiao <707610215@qq.com>
2020-09-05 14:25:44 +0800

    format code

--- mkdocs_roamlinks_plugin/plugin.py
+++ mkdocs_roamlinks_plugin/plugin.py
@@ -19,6 +19,7 @@ AUTOLINK_RE = r'\[([^\]]+)\]\((([^)/]+\.(md|png|jpg))(#.*)*)\)'
 #       4: |alias
 ROAMLINK_RE = r'\[\[(([^\]#\|]*)(#[^\|\]]+)*(\|[^\]]*)*)\]\]'
 
+
 class AutoLinkReplacer:
     def __init__(self, base_docs_url, page_url):
         self.base_docs_url = base_docs_url
@@ -28,8 +29,9 @@ class AutoLinkReplacer:
         # Name of the markdown file
         filename = match.group(3).strip()
 
-        # Absolute URL of the linker  # 看起来是page的dirtory文件夹
-        abs_linker_url = os.path.dirname(os.path.join(self.base_docs_url, self.page_url))
+        # Absolute URL of the linker
+        abs_linker_url = os.path.dirname(
+            os.path.join(self.base_docs_url, self.page_url))
 
         # Find directory URL to target link
         rel_link_url = ''
@@ -38,19 +40,23 @@ class AutoLinkReplacer:
             for name in files:
                 # If we have a match, create the relative path from linker to the link
                 if name == filename:
-                    # Absolute path to the file we want to link to  # 直接abs_link_url 等于root 就可以？还是需要转换路径？
+                    # Absolute path to the file we want to link to
                     abs_link_url = os.path.dirname(os.path.join(root, name))
                     # Constructing relative path from the linker to the link
-                    rel_link_url = os.path.join(os.path.relpath(abs_link_url, abs_linker_url), filename)
+                    rel_link_url = os.path.join(
+                        os.path.relpath(abs_link_url, abs_linker_url),
+                        filename)
         if rel_link_url == '':
-            print('WARNING: AutoLinksPlugin unable to find ' + filename + ' in directory ' + self.base_docs_url)
+            print('WARNING: AutoLinksPlugin unable to find ' + filename +
+                  ' in directory ' + self.base_docs_url)
             return match.group(0)
 
         # Construct the return link by replacing the filename with the relative path to the file
-        if(match.group(5) == None):
+        if (match.group(5) == None):
             link = match.group(0).replace(match.group(2), rel_link_url)
         else:
-            link = match.group(0).replace(match.group(2), rel_link_url + match.group(5))
+            link = match.group(0).replace(match.group(2),
+                                          rel_link_url + match.group(5))
 
         return link
 
@@ -65,7 +71,7 @@ class RoamLinkReplacer:
         if you want to link to png, make sure you filename contain suffix .png, same for other files
         but if you want to link to markdown, you don't need suffix .md """
         return re.sub(r"[\-_ ]", "", filename.lower()).replace(".md", "")
-    
+
     def gfm_anchor(self, title):
         """Convert to gfw title / anchor 
         see: https://gist.github.com/asabaylus/3071099#gistcomment-1593627"""
@@ -86,8 +92,9 @@ class RoamLinkReplacer:
         alias = match.group(4).strip('|') if match.group(4) else ""
         # print(f'--debug: link: {whole_link}, filename:{filename}, title: {title}, format_title: {format_title} alias:{alias}  ')
 
-        # Absolute URL of the linker 
-        abs_linker_url = os.path.dirname(os.path.join(self.base_docs_url, self.page_url))
+        # Absolute URL of the linker
+        abs_linker_url = os.path.dirname(
+            os.path.join(self.base_docs_url, self.page_url))
 
         # Find directory URL to target link
         rel_link_url = ''
@@ -97,21 +104,27 @@ class RoamLinkReplacer:
                 for name in files:
                     # If we have a match, create the relative path from linker to the link
                     if self.simplify(name) == self.simplify(filename):
-                    # if name == filename:
+                        # if name == filename:
                         # Absolute path to the file we want to link to
-                        abs_link_url = os.path.dirname(os.path.join(root, name))
+                        abs_link_url = os.path.dirname(os.path.join(
+                            root, name))
                         # Constructing relative path from the linker to the link
                         if title:
-                            rel_link_url = os.path.join(os.path.relpath(abs_link_url, abs_linker_url), name.replace('.md', ''), '#' + format_title)
+                            rel_link_url = os.path.join(
+                                os.path.relpath(abs_link_url, abs_linker_url),
+                                name.replace('.md', ''), '#' + format_title)
                         else:
-                            rel_link_url = os.path.join(os.path.relpath(abs_link_url, abs_linker_url), name)
+                            rel_link_url = os.path.join(
+                                os.path.relpath(abs_link_url, abs_linker_url),
+                                name)
             if rel_link_url == '':
-                print('WARNING: RoamLinksPlugin unable to find ' + filename + ' in directory ' + self.base_docs_url)
+                print('WARNING: RoamLinksPlugin unable to find ' + filename +
+                      ' in directory ' + self.base_docs_url)
                 return whole_link
         else:
-            rel_link_url = '#' + format_title 
-        
-        # Construct the return link 
+            rel_link_url = '#' + format_title
+
+        # Construct the return link
 
         if filename:
             if alias:
@@ -125,8 +138,12 @@ class RoamLinkReplacer:
 
 
 class RoamLinksPlugin(BasePlugin):
-
-    def on_page_markdown(self, markdown, page, config, site_navigation=None, **kwargs):
+    def on_page_markdown(self,
+                         markdown,
+                         page,
+                         config,
+                         site_navigation=None,
+                         **kwargs):
 
         # Getting the root location of markdown source files
         base_docs_url = config["docs_dir"]
@@ -135,7 +152,9 @@ class RoamLinksPlugin(BasePlugin):
         page_url = page.file.src_path
 
         # Look for matches and replace
-        markdown = re.sub(AUTOLINK_RE, AutoLinkReplacer(base_docs_url, page_url), markdown)
-        markdown = re.sub(ROAMLINK_RE, RoamLinkReplacer(base_docs_url, page_url), markdown)
+        markdown = re.sub(AUTOLINK_RE,
+                          AutoLinkReplacer(base_docs_url, page_url), markdown)
+        markdown = re.sub(ROAMLINK_RE,
+                          RoamLinkReplacer(base_docs_url, page_url), markdown)
 
         return markdown
