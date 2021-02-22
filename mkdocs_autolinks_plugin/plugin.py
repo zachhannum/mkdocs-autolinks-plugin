64a3b13c5f97294628437ed2409f5a9a98b34c2e
Joel Hedtjärn <joel.hedtjarn@gmail.com>
2021-02-22 02:24:32 +0100

    Optimize filename lookup
    The previous algorithm walked the entire docs directory for every link
    in every file, which quite expectedly took a lot of time. This update
    makes a dict of the filenames to their respective absolute paths, making
    the lookup O(1) instead of O(N), where N is the amount of files.
    
    This reduced the cumulative time spent in the plugin from 13 out of 19
    seconds, to 0.24 out of 7.0 seconds, when running `mkdocs build` with
    cProfile on unitystation/unitystation-wiki.

--- mkdocs_autolinks_plugin/plugin.py
+++ mkdocs_autolinks_plugin/plugin.py
@@ -22,30 +22,22 @@ AUTOLINK_RE = r'(?:\!\[\]|\[([^\]]+)\])\((([^)/]+\.(md|png|jpg|jpeg|bmp|gif))(#.
 
 
 class AutoLinkReplacer:
-    def __init__(self, base_docs_dir, abs_page_path):
+    def __init__(self, base_docs_dir, abs_page_path, filename_to_abs_path):
         self.base_docs_dir = base_docs_dir
         self.abs_page_path = abs_page_path
+        self.filename_to_abs_path = filename_to_abs_path
 
     def __call__(self, match):
-        # Name of the markdown file
+        # Name of the file
         filename = match.group(3).strip()
 
         # Absolute path to the directory of the linker
         abs_linker_dir = os.path.dirname(self.abs_page_path)
 
-        # Find the path to the target file
-        rel_link_path = None
-        # Walk through all files in docs directory to find a matching file
-        for root, dirs, files in os.walk(self.base_docs_dir):
-            for name in files:
-                # If we have a match, create the relative path from linker to the link
-                if name == filename:
-                    # Absolute path to the file we want to link to
-                    abs_link_path = os.path.join(root, name)
-                    # Constructing relative path from the linker to the link
-                    rel_link_path = os.path.relpath(abs_link_path, abs_linker_dir)
-
-        if rel_link_path is None:
+        # Look up the filename in the filename to absolute path lookup dict
+        try:
+            abs_link_path = self.filename_to_abs_path[filename]
+        except KeyError:
             LOG.warning(
                 "AutoLinksPlugin unable to find %s in directory %s",
                 filename,
@@ -53,7 +45,7 @@ class AutoLinkReplacer:
             )
             return match.group(0)
 
-        rel_link_path = quote(rel_link_path)
+        rel_link_path = quote(os.path.relpath(abs_link_path, abs_linker_dir))
         # Construct the return link by replacing the filename with the relative path to the file
         if match.group(5) is None:
             link = match.group(0).replace(match.group(2), rel_link_path)
@@ -64,7 +56,13 @@ class AutoLinkReplacer:
 
 
 class AutoLinksPlugin(BasePlugin):
-    def on_page_markdown(self, markdown, page, config, site_navigation=None, **kwargs):
+    def __init__(self):
+        self.filename_to_abs_path = None
+
+    def on_page_markdown(self, markdown, page, config, files, site_navigation=None, **kwargs):
+        # Initializes the filename lookiup dict if it hasn't already been initialized
+        if self.filename_to_abs_path is None:
+            self.init_filename_to_abs_path(files)
 
         # Getting the root location of markdown source files
         base_docs_dir = config["docs_dir"]
@@ -74,7 +72,25 @@ class AutoLinksPlugin(BasePlugin):
 
         # Look for matches and replace
         markdown = re.sub(
-            AUTOLINK_RE, AutoLinkReplacer(base_docs_dir, abs_page_path), markdown
+            AUTOLINK_RE,
+            AutoLinkReplacer(base_docs_dir, abs_page_path, self.filename_to_abs_path),
+            markdown,
         )
 
         return markdown
+
+    def init_filename_to_abs_path(self, files):
+        self.filename_to_abs_path = {}
+        for file_ in files:
+            filename = os.path.basename(file_.abs_src_path)
+
+            if filename in self.filename_to_abs_path:
+                LOG.warning(
+                    "Duplicate filename: '%s' exists at both '%s' and '%s'",
+                    filename,
+                    file_.abs_src_path,
+                    self.filename_to_abs_path[filename],
+                )
+                continue
+
+            self.filename_to_abs_path[filename] = file_.abs_src_path
